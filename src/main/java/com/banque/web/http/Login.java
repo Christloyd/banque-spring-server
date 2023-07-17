8cabe8b3a0b0884f2ba3f29d93add14b5bb33cee
Christloyd <svay.christloyd@gmail.com>
2023-07-17 16:45:43 +0200

    premier commit

---
+++ src/main/java/com/banque/web/http/Login.java
@@ -0,0 +1,22 @@
+package com.banque.web.http;
+
+public class Login {
+	
+	String unLogin ;
+	String unMdp;
+	
+	public String getUnLogin() {
+		return unLogin;
+	}
+	public void setUnLogin(String unLogin) {
+		this.unLogin = unLogin;
+	}
+	public String getUnMdp() {
+		return unMdp;
+	}
+	public void setUnMdp(String unMdp) {
+		this.unMdp = unMdp;
+	}
+	
+
+}
